fa344cc0a5ebadd7cd744613909003e52fcbb045
kartikcpp <kartiksaini8881@gmail.com>
2021-02-20 17:41:41 +0530

    addded firebase auth with google

--- app/src/main/java/com/kartiksaini/notesapp/MainActivity.java
+++ app/src/main/java/com/kartiksaini/notesapp/MainActivity.java
@@ -1,28 +1,54 @@
 package com.kartiksaini.notesapp;
 
+import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
+import com.bumptech.glide.Glide;
+import com.firebase.ui.auth.AuthUI;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    
+    private static final int RC_SIGN_IN = 350;
+    FirebaseAuth mfirebaseauth;
+    FirebaseUser user;
+    ImageView logouser;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mfirebaseauth=FirebaseAuth.getInstance();
 
-        notsignin();
+       notsignin(mfirebaseauth);
+       logouser=findViewById(R.id.logouser);
         Toolbar toolbar = findViewById(R.id.toolbar);
+
+        Glide.with(this).load(user.getPhotoUrl().toString()).circleCrop().into(logouser);
+
+        Log.i("murl",user.getPhotoUrl().toString());
+
+
+        logouser.setImageURI(user.getPhotoUrl());
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
@@ -35,9 +61,40 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void notsignin() {
+    private void notsignin(FirebaseAuth mfirebaseauth) {
+        if (mfirebaseauth.getCurrentUser()==null){
+            // Choose authentication providers
+//            List<AuthUI.IdpConfig> providers = Arrays.asList(
+//                    new AuthUI.IdpConfig.EmailBuilder().build(),
+//                    new AuthUI.IdpConfig.PhoneBuilder().build(),
+//                    new AuthUI.IdpConfig.GoogleBuilder().build(),
+//                    new AuthUI.IdpConfig.FacebookBuilder().build(),
+//                    new AuthUI.IdpConfig.TwitterBuilder().build());
+
+            List<AuthUI.IdpConfig> providers= Arrays.asList(
+                    new AuthUI.IdpConfig.GoogleBuilder().build()
+            );
 
 
+//            startActivityForResult(
+//                    AuthUI.getInstance()
+//                            .createSignInIntentBuilder()
+//                            .setAvailableProviders(providers)
+//                            .build(),
+//                    RC_SIGN_IN);
+            startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(providers).build(),RC_SIGN_IN);
+        }
+        else{user=FirebaseAuth.getInstance().getCurrentUser();}
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==RC_SIGN_IN){
+            if (resultCode==RESULT_OK){
+                 user=FirebaseAuth.getInstance().getCurrentUser();
+            }
+        }
     }
 
     @Override
